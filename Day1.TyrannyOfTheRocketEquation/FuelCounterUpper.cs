2495a6b6a79935f62389ec5062056ab909859943
Nicholas Booth <nicholas.booth@meritec.co.uk>
2019-12-05 10:24:48 +0000

    Day1.TyrannyOfTheRocketEquation

---
+++ Day1.TyrannyOfTheRocketEquation/FuelCounterUpper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1.TyrannyOfTheRocketEquation
+{
+    internal static class FuelCounterUpper
+    {
+        public static void Main(string[] args)
+        {
+            //Import module masses from disk
+            var moduleMasses = GetModulesFromFile("E:/repos/code-advent/Day1.TyrannyOfTheRocketEquation/Modules.csv");
+            //Get a list of SpaceCraft with fuel info
+            var listOfSpacecraft = moduleMasses.Select(mass => new Spacecraft(mass)).ToList();
+            //Find the total fuel required for all
+            var totalFuel = listOfSpacecraft.Sum(spacecraft => spacecraft.FuelRequired);
+            Console.WriteLine("Total fuel required for Santa's Elves: "+ totalFuel);
+        }
+
+        private static IEnumerable<int> GetModulesFromFile(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+
+            return lines.Select(int.Parse).ToList();
+        }
+    }
+}
---
+++ Day1.TyrannyOfTheRocketEquation/Spacecraft.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day1.TyrannyOfTheRocketEquation
+{
+    public class Spacecraft
+    {
+        public int FuelRequired { get; }
+        public int ModuleMass { get; }
+
+        public Spacecraft(int moduleMass)
+        {
+            ModuleMass = moduleMass;
+            //Take mass, divide by three, round down, minus 2
+            FuelRequired = (int) Math.Floor((float)moduleMass / 3f) - 2;
+        }
+    }
+}
