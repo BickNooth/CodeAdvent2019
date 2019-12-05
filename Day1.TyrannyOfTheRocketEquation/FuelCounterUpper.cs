d2b695c900933f69955147e27acb89dada6f1e78
Nick Booth <nick-booth@live.com>
2019-12-05 17:19:19 +0000

    Part 2: Accounting Weight

--- Day1.TyrannyOfTheRocketEquation/FuelCounterUpper.cs
+++ Day1.TyrannyOfTheRocketEquation/FuelCounterUpper.cs
@@ -9,13 +9,28 @@ namespace Day1.TyrannyOfTheRocketEquation
     {
         public static void Main(string[] args)
         {
+            #region Part 1
+
             //Import module masses from disk
-            var moduleMasses = GetModulesFromFile("E:/repos/code-advent/Day1.TyrannyOfTheRocketEquation/Modules.csv");
+            var moduleMasses = GetModulesFromFile(Environment.GetEnvironmentVariable("ModulesCsvLocation"));
             //Get a list of SpaceCraft with fuel info
             var listOfSpacecraft = moduleMasses.Select(mass => new Spacecraft(mass)).ToList();
             //Find the total fuel required for all
             var totalFuel = listOfSpacecraft.Sum(spacecraft => spacecraft.FuelRequired);
-            Console.WriteLine("Total fuel required for Santa's Elves: "+ totalFuel);
+            Console.WriteLine("Total fuel required for Santa's Elves (P1): " + totalFuel.ToString("N0"));
+
+            #endregion
+
+            #region Part 2
+
+            //Get a list of SpaceCraft with fuel info, account for the weight of additional fuel
+            var spacecraftAccountingFuel = moduleMasses.Select(mass => new Spacecraft(mass, true)).ToList();
+            //Find the total fuel required for all
+            var totalFuelWithFuel = spacecraftAccountingFuel.Sum(spacecraft => spacecraft.FuelRequired);
+            Console.WriteLine("Total fuel required for Santa's Elves, accounting for additional weight (P2): " +
+                              totalFuelWithFuel.ToString("N0"));
+
+            #endregion
         }
 
         private static IEnumerable<int> GetModulesFromFile(string filePath)
--- Day1.TyrannyOfTheRocketEquation/Spacecraft.cs
+++ Day1.TyrannyOfTheRocketEquation/Spacecraft.cs
@@ -4,14 +4,44 @@ namespace Day1.TyrannyOfTheRocketEquation
 {
     public class Spacecraft
     {
-        public int FuelRequired { get; }
+        public int FuelRequired { get; set; }
         public int ModuleMass { get; }
+        
 
         public Spacecraft(int moduleMass)
         {
             ModuleMass = moduleMass;
             //Take mass, divide by three, round down, minus 2
             FuelRequired = (int) Math.Floor((float)moduleMass / 3f) - 2;
+        }
+
+        public Spacecraft(int moduleMass, bool accountForFuelWeight)
+        {
+            ModuleMass = moduleMass;
+
+            var fuelRequired = 0;
+            var remainderAmount = moduleMass;
+            var remainder = true;
+            while (remainder)
+            {
+                var fuel = CalculateFuelForMass(remainderAmount);
+                if (fuel > 0)
+                {
+                    fuelRequired += fuel;
+                    remainderAmount = fuel;
+                }
+                else
+                {
+                    remainder = false;
+                }
+            }
+
+            FuelRequired = fuelRequired;
+        }
+
+        private int CalculateFuelForMass(int weight)
+        {
+            return (int) Math.Floor((float)weight / 3f) - 2;
         }
     }
 }
