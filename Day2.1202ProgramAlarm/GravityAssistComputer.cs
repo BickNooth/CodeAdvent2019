1e505bd1907a665164abfebc72bb18e6968ee8dc
Nick Booth <nick-booth@live.com>
2019-12-06 07:58:39 +0000

    Day 2 scaffolding

---
+++ Day2.1202ProgramAlarm/GravityAssistComputer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace Day2._1202ProgramAlarm
+{
+    internal static class GravityAssistComputer
+    {
+        public static void Main(string[] args)
+        {
+            var intCodes = new Intcode(Environment.GetEnvironmentVariable("InputTxtLocation"));
+            
+            intCodes.ProcessProgram();
+        }
+        
+        private static IEnumerable<int> GetIntcodeFromFile(string filePath)
+        {
+            var fileText = File.ReadAllText(filePath);
+            var itemArray = fileText.Split(',');
+            return itemArray.Select(int.Parse).ToList();
+        }
+    }
+}
---
+++ Day2.1202ProgramAlarm/Intcode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting;
+
+namespace Day2._1202ProgramAlarm
+{
+    public class Intcode
+    {
+        public List<int> Ints { get; }
+
+        public Intcode(string filePath)
+        {
+            var fileText = File.ReadAllText(filePath);
+            var itemArray = fileText.Split(',');
+            Ints = itemArray.Select(int.Parse).ToList();
+        }
+
+        public void AddOpCode(int firstNo, int secondNo, int replaceNo)
+        {
+            Ints[replaceNo] = Ints[firstNo] + Ints[secondNo];
+        }
+
+        public void MultiplyOpCode(int firstNo, int secondNo, int replaceNo)
+        {
+            Ints[replaceNo] = Ints[firstNo] * Ints[secondNo];
+        }
+
+        public void ProcessProgram()
+        {
+            var index = 0;
+            var opCode = 0;
+            while (opCode != 99)
+            {
+                switch (Ints[index])
+                {
+                    case 1:
+                        AddOpCode(Ints[index+1],Ints[index+2],Ints[index+3]);
+                        break;
+                    case 2:
+                        MultiplyOpCode(Ints[index+1],Ints[index+2],Ints[index+3]);
+                        break;
+                    case 99:
+                        opCode = 99;
+                        break;
+                }
+
+                index += 4;
+            }
+        }
+    }
+}
